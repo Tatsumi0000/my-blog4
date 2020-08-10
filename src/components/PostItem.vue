fd5615454b9759399dc86e2cfbe600cf5e846159
Tatsumi0000 <ti18027@student.miyazaki-u.ac.jp>
2020-08-11 00:06:23 +0900

    Add: カードで表示するためのprops

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -5,9 +5,24 @@
 <script>
 export default {
   props: {
+    // 記事タイトル
+    title: {
+      type: String,
+      default: 'title',
+    },
+    // サムネのURL
+    thumbnailUrl: {
+      type: String,
+      default: 'thumbnail_url',
+    },
+    // 記事の作成日
+    createdDate: {
+      type: Date,
+      default: '2000-01-01',
+    },
+    // 記事の本文
     posts: {
       type: String,
-      required: false,
       default: 'A',
     },
   },
